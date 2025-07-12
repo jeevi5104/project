5bf422069116412bd165a898b1778a8f65acd237
jeevi5104 <jeevithamurugesan10@gmail.com>
2025-07-12 14:53:05 +0530

    Add files via upload

---
+++ pg password genrator.py
@@ -0,0 +1,7 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> KeyboardInterrupt
+>>> 
+====================================================== RESTART: C:/Users/navee/AppData/Local/Programs/Python/Python313/password.py =====================================================
+Enter password length: 23
+Generated Password: ~ropTtyD%tGERmVhW@{I;rA
